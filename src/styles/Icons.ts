6ec4ce715d8c11859d90dd50dc17f185c1830245
Gustavo Volke <gustavo_volke@hotmail.com>
2024-10-15 22:29:08 -0300

    Finishing profile and starting tweets feed

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -1,3 +1,5 @@
+// Exemplo interessante de importação de ícones. Todos os ícones que vão ser usados na aplicação estão concentrados nesse arquivo. Conforme for precisando dos icones nos componentes a gente vai importando eles diretamente dessa página
+
 export {
   Home,
   Notifications,
@@ -10,3 +12,4 @@ export {
 export { Person, Cake, Favorite } from "styled-icons/material";
 export { ArrowLeft } from "styled-icons/heroicons-solid";
 export { ChatBubbleLeft } from "styled-icons/heroicons-outline";
+export { RocketseatIcon as Rocketseat } from "../components/RocketseatIcon";
